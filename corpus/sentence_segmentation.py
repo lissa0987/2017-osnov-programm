5a652d41d119e4ef6ca4d8f1d2ef880bdb95892c
lissa0987 <noborisenko@gmail.com>
2017-09-23 11:11:14 +0300

    add sentence_segmantation

---
+++ corpus/sentence_segmentation.py
@@ -0,0 +1,25 @@
+import sys
+text = sys.stdin.read()
+
+text.translate('\n')
+punc = ['.', '?', '!']
+sentence = []
+sentence_start = 0
+sentence_end = 0
+for i in range(len(text)-2):
+    
+    if text[i] in punc and text[i + 1] == ' ' and text[i-2].isalnum():#check the end of the sentence
+        sentence_end = i + 1
+        sentence.append(text[sentence_start:sentence_end])
+        sentence_start = sentence_end + 1
+
+    if (text[i] == '"' or text[i] == '»') and text[i-1] in punc and text[i-2].isalnum()#exeptions
+        m = text[i]
+        sentence_end = i + 1
+        sentence.append(text[sentence_start:sentence_end])
+        sentence_start = sentence_end + 1
+
+sentence.append(text[sentence_start:len(text)])
+for i in sentence:
+	n = sys.stdout.write(i)
+print(n)
